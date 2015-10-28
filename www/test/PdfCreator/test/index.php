80e8ed86d08086cde135f7e6f2240072a9ebf264
alamzin <lamzin80@mail.ru>
2015-10-28 23:53:09 +0300

    #mrcs graphisc+

---
+++ www/test/PdfCreator/test/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once dirname(__FILE__) . '/../DssParser.php';
+
+$filename = dirname(__FILE__) . '/u539f344461.dss';
+//$filename = dirname(__FILE__) . '/1pattern.dss';
+$parser = new DssParser();
+$parser->parseData($filename);
