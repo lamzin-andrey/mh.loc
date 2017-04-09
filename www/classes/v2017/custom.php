c041e8ce0293d1a71d10e9a9b3f30ac0a79b5037
Andrey Lamzin <lamzin.an@gmail.com>
2017-04-09 11:59:11 +0300

    полезные новшества в v2017

---
+++ www/classes/v2017/custom.php
@@ -0,0 +1,13 @@
+<?php
+/*define('DB_HOST', 'mysql.hostinger.ru');
+define('DB_USER', 'u277521421_stat');
+define('DB_PASSWORD', 'juHyXu');
+define('DB_NAME', 'u277521421_stat');
+define('ADMIN_EMAIL', 'admin@fastxampp.org');*/
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_NAME', 'mh');
+define('ADMIN_EMAIL', 'lamzin.an@gmail.com');/**/
